0cbf08c686468d7280b68e2c0b22b59c935ee4fb
JCharis Jesse <jcharistech@gmail.com>
2019-02-21 10:09:08 +0200

    Document Redaction and Sanitization Web App with Spacy and Flask

---
+++ Document_Redaction_&_Sanitization_Web_App_With_Spacy/app.py
@@ -0,0 +1,81 @@
+from flask import Flask,url_for,render_template,request,send_file
+from flask_uploads import UploadSet,configure_uploads,ALL,DATA
+from werkzeug import secure_filename
+
+# Other Packages
+import os
+import spacy
+nlp = spacy.load('en')
+
+# Initialize App
+app = Flask(__name__)
+# Configuration For Uploads
+files = UploadSet('files',ALL)
+app.config['UPLOADED_FILES_DEST'] = 'static/uploadedfiles'
+configure_uploads(app,files)
+
+# Functions to Sanitize and Redact 
+def sanitize_names(text):
+    docx = nlp(text)
+    redacted_sentences = []
+    for ent in docx.ents:
+        ent.merge()
+    for token in docx:
+        if token.ent_type_ == 'PERSON':
+            redacted_sentences.append("[REDACTED]")
+        else:
+            redacted_sentences.append(token.string)
+    return "".join(redacted_sentences)
+
+def sanitize_places(text):
+    docx = nlp(text)
+    redacted_sentences = []
+    for ent in docx.ents:
+        ent.merge()
+    for token in docx:
+        if token.ent_type_ == 'GPE':
+            redacted_sentences.append("[REDACTED]")
+        else:
+            redacted_sentences.append(token.string)
+    return "".join(redacted_sentences)
+
+
+		
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/sanitize',methods=['GET','POST'])
+def sanitize():
+	if request.method == 'POST':
+		choice = request.form['taskoption']
+		rawtext = request.form['rawtext']
+		if choice == 'redact_names':
+			result = sanitize_names(rawtext)
+		elif choice == 'places':
+			result = sanitize_places(rawtext)
+		else:
+			result = sanitize_names(rawtext)
+	return render_template('index.html',rawtext=rawtext,result=result)
+
+
+@app.route('/uploads',methods=['GET','POST'])
+def uploads():
+	if request.method == 'POST' and 'txt_data' in request.files:
+		file = request.files['txt_data']
+		filename = secure_filename(file.filename)
+		file.save(os.path.join('static/uploadedfiles',filename))
+
+		# Document Redaction Here
+		with open(os.path.join('static/uploadedfiles',filename),'r+') as f:
+			myfile = f.read()
+			result = sanitize_names(myfile)
+		
+
+
+	return render_template('result.html',filename=filename,result=result,myfile=myfile)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
