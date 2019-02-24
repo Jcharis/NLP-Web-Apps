79a22aa85c4e54225f9a1fca499e065cfdc97811
JCharis Jesse <jcharistech@gmail.com>
2019-02-24 07:27:45 +0200

    Document Redaction and Sanitization with Spacy + Downloads option

--- Document_Redaction_&_Sanitization_Web_App_With_Spacy/app.py
+++ Document_Redaction_&_Sanitization_Web_App_With_Spacy/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for,render_template,request,send_file
+from flask import Flask,url_for,render_template,request,send_file,redirect
 from flask_uploads import UploadSet,configure_uploads,ALL,DATA
 from werkzeug import secure_filename
 
@@ -6,6 +6,9 @@ from werkzeug import secure_filename
 import os
 import spacy
 nlp = spacy.load('en')
+
+import time
+timestr = time.strftime("%Y%m%d-%H%M%S")
 
 # Initialize App
 app = Flask(__name__)
@@ -22,7 +25,7 @@ def sanitize_names(text):
         ent.merge()
     for token in docx:
         if token.ent_type_ == 'PERSON':
-            redacted_sentences.append("[REDACTED]")
+            redacted_sentences.append("[REDACTED NAME]")
         else:
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
@@ -34,11 +37,39 @@ def sanitize_places(text):
         ent.merge()
     for token in docx:
         if token.ent_type_ == 'GPE':
+            redacted_sentences.append("[REDACTED PLACE]")
+        else:
+            redacted_sentences.append(token.string)
+    return "".join(redacted_sentences)
+
+def sanitize_date(text):
+    docx = nlp(text)
+    redacted_sentences = []
+    for ent in docx.ents:
+        ent.merge()
+    for token in docx:
+        if token.ent_type_ == 'DATE':
+            redacted_sentences.append("[REDACTED DATE]")
+        else:
+            redacted_sentences.append(token.string)
+    return "".join(redacted_sentences)
+
+def sanitize_org(text):
+    docx = nlp(text)
+    redacted_sentences = []
+    for ent in docx.ents:
+        ent.merge()
+    for token in docx:
+        if token.ent_type_ == 'ORG':
             redacted_sentences.append("[REDACTED]")
         else:
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
 
+def writetofile(text):
+	file_name = 'yourdocument' + timestr + '.txt'
+	with open(os.path.join('static/downloadfiles',file_name),'w') as f:
+		f.write(text)
 
 		
 @app.route('/')
@@ -54,6 +85,10 @@ def sanitize():
 			result = sanitize_names(rawtext)
 		elif choice == 'places':
 			result = sanitize_places(rawtext)
+		elif choice == 'date':
+			result = sanitize_date(rawtext)
+		elif choice == 'org':
+			result = sanitize_org(rawtext)
 		else:
 			result = sanitize_names(rawtext)
 	return render_template('index.html',rawtext=rawtext,result=result)
@@ -63,6 +98,7 @@ def sanitize():
 def uploads():
 	if request.method == 'POST' and 'txt_data' in request.files:
 		file = request.files['txt_data']
+		choice = request.form['saveoption']
 		filename = secure_filename(file.filename)
 		file.save(os.path.join('static/uploadedfiles',filename))
 
@@ -70,12 +106,23 @@ def uploads():
 		with open(os.path.join('static/uploadedfiles',filename),'r+') as f:
 			myfile = f.read()
 			result = sanitize_names(myfile)
+		if choice == 'savetotxt':
+			new_res = writetofile(result)
+			return redirect(url_for('downloads'))
+		elif choice == 'no_save':
+			pass
+		else:
+			pass
+
 		
 
 
 	return render_template('result.html',filename=filename,result=result,myfile=myfile)
 
-
+@app.route('/downloads')
+def downloads():
+	files = os.listdir(os.path.join('static/downloadfiles'))
+	return render_template('downloadsdirectory.html',files=files)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- Document_Redaction_&_Sanitization_Web_App_With_Spacy/static/js/main.js
+++ Document_Redaction_&_Sanitization_Web_App_With_Spacy/static/js/main.js
@@ -3,5 +3,14 @@ function copyToClipboard(element) {
   $("body").append($temp);
   $temp.val($(element).text()).select();
   document.execCommand("copy");
+  alert("Result Copied");
   $temp.remove();
 }
+
+// function copyToClipboard() {
+//   var copyText = document.getElementById("p1");
+//   copyText.select();
+//   document.execCommand("copy");
+//   alert("Copied the result ");
+//   console.log("Copied");
+// }
